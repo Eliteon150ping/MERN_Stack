0fe22af357c1fef38e78e28a40ad9963b8996b6e
Eliteon150ping <rishensurjoobhalee@gmail.com>
2024-08-03 21:50:00 +0200

    Reuploaded

---
+++ Projects/Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://R1:R1234@cluster0.q77bea6.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
